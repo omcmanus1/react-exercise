af88ac82cfdb685aa5c021c705a663e1818ff948
Oliver McManus <omcmanus1@gmail.com>
2023-04-07 12:38:47 +0100

    axios setup, dependency intallation

---
+++ api.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const fakeApi = axios.create({
+  baseURL: "https://fakestoreapi.com/products",
+});
+
+export const fetchProducts = () => {
+  return fakeApi.get().then((res) => {
+    console.log(res.data)
+  })
+}
--- src/Components/ProductsPage.jsx
+++ src/Components/ProductsPage.jsx
@@ -1,3 +1,4 @@
+
 export default function ProductsPage() {
   return <h1>ALL PRODUCTS</h1>
 }

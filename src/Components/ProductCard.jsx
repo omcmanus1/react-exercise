ba1f9a0a34f542cec3cff0e4935b05bdcc7411a4
Oliver McManus <omcmanus1@gmail.com>
2023-04-07 15:34:09 +0100

    button functionality

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 import {
+  Button,
   Card,
   CardContent,
   CardHeader,
@@ -7,18 +10,19 @@ import {
 } from "@mui/material";
 
 export default function ProductCard({ product }) {
+  const [itemAdded, setItemAdded] = useState(false);
+  console.log(itemAdded);
+
+  const handleButtonClick = () => setItemAdded(!itemAdded);
+
   return (
     <Card className="product-card" raised={true}>
-      <CardContent>
+      <CardContent sx={{ alignContent: "center", justifyContent: "center" }}>
         <CardHeader
           className="product-image"
           title={product.title}
           subheader={`£${product.price}`}
         />
-        <Typography variant="body1">Category: {product.category}</Typography>
-        <Typography variant="body1">
-          Rating: {product.rating.rate} ({product.rating.count} votes)
-        </Typography>
         <CardMedia
           className="product-image"
           component="img"
@@ -26,9 +30,38 @@ export default function ProductCard({ product }) {
           alt={product.title}
           sx={{ height: 140, objectFit: "contain", padding: 2 }}
         />
-        <Typography variant="body2" sx={{ paddingLeft: 5, paddingRight: 5 }}>
+        <Typography variant="body1">Category: {product.category}</Typography>
+        <Typography variant="body1">
+          Rating: {product.rating.rate} ({product.rating.count} votes)
+        </Typography>
+        <Typography
+          variant="body2"
+          sx={{
+            paddingTop: 2,
+            paddingBottom: 2,
+            paddingLeft: 5,
+            paddingRight: 5,
+          }}
+        >
           {product.description}
         </Typography>
+        {itemAdded ? (
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "darkred" }}
+            onClick={handleButtonClick}
+          >
+            Remove From Cart
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "darkgreen" }}
+            onClick={handleButtonClick}
+          >
+            Add To Cart
+          </Button>
+        )}
       </CardContent>
     </Card>
   );

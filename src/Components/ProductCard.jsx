927e285dbe38391444c2f158eb064da1287c73c0
Oliver McManus <omcmanus1@gmail.com>
2023-04-07 14:06:51 +0100

    image on cards + resizing

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,5 +1,22 @@
-import { Card } from "@mui/material";
+import { Card, CardContent, CardHeader, CardMedia } from "@mui/material";
 
 export default function ProductCard({ product }) {
-  return <Card className="product-card">{product.title}</Card>;
+  return (
+    <Card className="product-card" raised={true}>
+      <CardContent>
+        <CardHeader
+          className="product-image"
+          title={product.title}
+          subheader={`£${product.price}`}
+        />
+        <CardMedia
+          className="product-image"
+          component="img"
+          image={product.image}
+          alt={product.title}
+          sx={{ height: 140, objectFit: "contain" }}
+        />
+      </CardContent>
+    </Card>
+  );
 }
